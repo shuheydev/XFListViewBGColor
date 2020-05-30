b03eaa7f5b48ad14e3f75997e99628ccb5cd1db8
Shuhey <shuhey.dev@gmail.com>
2020-05-30 19:08:18 +0900

    Add project files.

---
+++ XFListViewBGColor/XFListViewBGColor/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XFListViewBGColor
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public ObservableCollection<string> Items { get; set; }
+        public MainPage()
+        {
+            InitializeComponent();
+
+            this.BindingContext = this;
+
+            this.Items = new ObservableCollection<string>
+            {
+                "Item 1",
+                "Item 2",
+                "Item 3",
+                "Item 4",
+                "Item 5",
+                "Item 6",
+                "Item 7",
+            };
+        }
+    }
+}
